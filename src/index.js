3c4fbdbe43666faca8f383af0b970d0d58135cf5
Viacheslav Dziubenko <97623457+MrViachesav25@users.noreply.github.com>
2023-08-07 21:27:44 +0300

    add com

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/es/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* За допомогою persistor дані з локального сховища попадають в стейт */}
-    <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
         <App />
       </Provider>
-    </PersistGate>
   </React.StrictMode>
 );
